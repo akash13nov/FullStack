e7eb7540e606cc02c9c939cfb0c3376b21ea4ef3
akash13nov <akash13nov@gmail.com>
2026-02-04 20:19:47 +0530

    fullstack D-93

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -39,7 +39,7 @@ function App() {
         <input name="title" type="text" placeholder="Title" />
         <input name="description" type="text" placeholder="Description" />
         <input name="comment" type="text" placeholder="Comment" />
-        <button className="update-btn">Create note</button>
+        <button className="create-btn">Create note</button>
       </form>
 
       <div className="notes">
@@ -49,6 +49,7 @@ function App() {
               <h3>{note.title}</h3>
               <p>{note.description}</p>
               <p>{note.comment}</p>
+              <button className="update-btn">Update</button>
               <button
                 className="delete-btn"
                 onClick={() => {
